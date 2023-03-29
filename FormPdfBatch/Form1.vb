f494749e98d128046c6127769f4c3e231aa8f071
hurz21 <dr.j.feinen@gmail.com>
2023-03-29 12:15:25 +0200

    FULLNAME

--- FormPdfBatch/Form1.vb
+++ FormPdfBatch/Form1.vb
@@ -2034,7 +2034,7 @@ Public Class Form1
 
         If vid = "fehler" Then End
         Dim Sql As String
-        Sql = "SELECT * FROM dokumente where   dokumentid<2000000 and dokumentid>0  " &
+        Sql = "SELECT * FROM dokumente where   dokumentid<2000000 and dokumentid>0   " &
                   "  order by dokumentid desc "
         DT = alleDokumentDatenHolen(Sql)
         'teil1 = pdf -----------------------------------------------
